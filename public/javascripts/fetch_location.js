de2e21b594873f134a5540aa29ab4f56977f1187
Lia Skalkos <hello@liaskalkos.net>
2015-07-15 12:31:22 -0400

    Complete

--- public/javascripts/fetch_location.js
+++ public/javascripts/fetch_location.js
@@ -2,6 +2,15 @@ $(document).ready(function(){
 
   function getLocation() {
     // code here
+
+    navigator.geolocation.getCurrentPosition(function(position) {
+
+      $('p#longitude').html(position.coords.longitude);
+      $('p#latitude').html(position.coords.latitude);
+
+      insertMap(position.coords.latitude, position.coords.longitude);
+
+    });
   }
 
   getLocation();
--- public/javascripts/insert_map.js
+++ public/javascripts/insert_map.js
@@ -1,3 +1,10 @@
 function insertMap(latitude, longitude) {
   // code here
+    var mapCanvas = document.getElementById('map_canvas');
+    var mapOptions = {
+        center: new google.maps.LatLng(latitude, longitude),
+        zoom: 8,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+    var map = new google.maps.Map(mapCanvas, mapOptions);
 }
